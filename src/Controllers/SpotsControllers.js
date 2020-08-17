f599c78c4718ee203c67289378d10f08ec2ed447
GustavoOliveira57 <guto.oliveira01@gmail.com>
2020-08-17 17:30:15 -0300

    FIrst Commit

---
+++ src/Controllers/SpotsControllers.js
@@ -0,0 +1,10 @@
+const { store } = require("./SessionController");
+
+module.exports ={
+    async store(req, res) {
+        console.log(req.body);
+        console.log(req.file);
+        
+        return res.json({ ok: true });
+    }
+};
